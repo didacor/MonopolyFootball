d777bc63e2c5fdf2029bd6c69d8a721c3c5a13d9
didac <dorri.dam@institutcampalans.net>
2025-05-08 16:17:17 +0200

    a

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ document.getElementById("loginForm").addEventListener("submit", async function(e
     const contrasenya = document.getElementById("contrasenya").value;
 
     try {
-        const response = await fetch("https://ellaboratori.cat/api/login", {
+        const response = await fetch("/api/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             credentials: "include",
